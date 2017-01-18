b7578da911010ee9a80bd0626cc9900ef9776efc
Tanbui <hoangtan223@gmail.com>
2017-01-18 21:05:46 +0700

    add directive for dropdow

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -6,9 +6,11 @@ import { Recipe } from '../recipe';
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe("Dummy", "Dummy", "http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg",[]),
+    new Recipe("Buth", "Buth", "http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg",[])
+  ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
-  recipe = new Recipe("Dummy", "Dummy", "http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg");
   
   constructor() { }
 
---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,16 @@
+import { Ingredient } from './../recipes/ingredient';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rb-shopping-list',
+  templateUrl: './shopping-list.component.html'
+})
+export class ShoppingListComponent implements OnInit {
+  items: Ingredient[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
